6b3c07c21a2840ca123834ea4f2abd2329f34823
szantomiso <h378047@stud.u-szeged.hu>
2025-05-07 16:16:30 +0200

    main might be working like this, needs to be tested

--- main.py
+++ main.py
@@ -29,3 +29,25 @@ async def send_message(message: Message, user_message: str) -> None:
         await message.author.send(response) if is_private else message.channel.send(response)
     except Exception as e:
         print(e)
+
+@client.event
+async def on_ready() -> None:
+    print(f"{client.user} is now running!")
+
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f"[{channel}] {username}: {user_message}")
+    await send_message(message, user_message)
+
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == "__main__":
+    main()
